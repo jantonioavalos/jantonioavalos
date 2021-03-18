003a169967f5ebd713847d9fa9be9d7b534e4c32
Antonio Avalos <a.avalos@blackstone.studio>
2021-03-18 15:37:10 -0600

    Add blog and mentoring

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,30 +1,37 @@
 import React from "react";
 import { Link } from "gatsby";
-import { RiMenu3Line, RiCloseLine, RiExternalLinkLine } from "react-icons/ri";
+import { RiMenu3Line, RiCloseLine, RiExternalLinkLine, RiDownloadCloud2Line } from "react-icons/ri";
 
 const CV = "https://drive.google.com/file/d/1ZfNZ1HIsfiVUxY3YC9FSLt4bQMf6A0X6/view?usp=sharing";
+const Mentoring = "https://mentoring.jantonioavalos.com";
+const Blog = "https://blog.jantonioavalos.com";
 
 const MenuItems = [
   {
     path: "/",
-    title: "Home",
+    title: "Work",
   },
   {
     path: "/about",
     title: "About",
   },
-    {
-    path: "/blog",
+  {
+    path: Blog,
     title: "Blog",
+  },
+  {
+    path: Mentoring,
+    title: "Mentoring",
   },
   {
     path: "/resources",
     title: "Resources",
   },
+  
   // {
   //   path: CV,
   //   title: "Resume",
-  // }
+  // },
 ];
 
 const ListLink = (props) => (
@@ -64,16 +71,19 @@ class Navigation extends React.Component {
         >
           <div className="icon-menu-line">
             <RiMenu3Line />
+            {/* <small>Menu</small> */}
           </div>
           <div className="icon-menu-close">
             <RiCloseLine />
           </div>
         </button>
         <ul>{listMenuItems}</ul>
-        {/* <Link to={CV} target="_blank" className="button -banner">
+        {/* Button
+        <Link to={CV} target="_blank" className="button -banner">
           <RiDownloadCloud2Line className="label" />
           Resume
-        </Link> */}
+        </Link> 
+        */}
       </nav>
     );
   }
